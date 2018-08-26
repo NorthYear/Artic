a965af3225c63cd1a336645eb500a8ab4c3bb045
Josh Miller <wickedhillsmedia@gmail.com>
2018-08-26 10:44:24 -0400

    Added Is to the declaration... possible use for it later... don't know yet

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Database } from "./database";
 import { Entity } from "./entity";
+import { Is } from "./utils/is";
 
 export { Database };
 export { Entity };
+export { Is }
 
-export default { Database, Entity }
+export default { Database, Entity, Is }

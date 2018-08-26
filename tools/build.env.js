5a6209b7851b4ff41710c7e4e2a84eb92b2026f0
Josh Miller <wickedhillsmedia@gmail.com>
2018-08-26 00:08:18 -0400

    built the script create the testing env file

---
+++ tools/build.env.js
@@ -0,0 +1,19 @@
+const crypto = require("crypto");
+const fs = require("fs");
+const path = require("path");
+const key = crypto.randomBytes(24).toString("base64");
+
+
+function writeEnv(properties) {
+    let output = "";
+    for(let iterator in properties) {
+        output += `${iterator}=${properties[iterator]}\n`
+    }
+    return output;
+}
+
+let properties = {
+    "ENCRYPTION_KEY" : key
+}
+
+fs.writeFileSync(".env", writeEnv(properties), "utf8");

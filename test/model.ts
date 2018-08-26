54667ccb00bcdabcaacfe8de058cc4f33729ac29
Josh Miller <wickedhillsmedia@gmail.com>
2018-08-26 00:24:28 -0400

    setup a basic model for testing

--- test/model.ts
+++ test/model.ts
@@ -1,1 +1,19 @@
 require('dotenv').config()
+import { Database, Entity } from "../src/index";
+
+export class Article extends Entity {
+    public title: string;
+    public content: string;
+}
+
+const context = {
+    Article
+}
+
+export const mainDB = Database.make("main", {
+    context
+})
+
+export const backupDB = Database.make("backup", {
+    context
+})

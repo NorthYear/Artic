dcd9eadd5799d782e0a5a5b54b5964f6c33e7f87
Josh Miller <wickedhillsmedia@gmail.com>
2018-08-28 19:01:13 -0400

    Updated the exception module with fixes and commenting

--- src/utils/exceptions.ts
+++ src/utils/exceptions.ts
@@ -1,19 +1,61 @@
+/**
+ * ### @Artic / Utils / Exceptions
+ * 
+ * Module for handle errors/exceptions
+ */
 export namespace Exceptions {
 
+    /**
+     * ### @Artic / Utils / Exceptions / View
+     * 
+     * Template for a nice user readable exception 
+     * @param title 
+     * @param msgs 
+     */
     export function view(title: string, ...msgs) {
-        let list = msgs.map(msg => "+ " + msg);
-        list.unshift("--------------------------------");
-        list.unshift(title);
-        return list.join("\n");
+        msgs.unshift("--------------------------------");
+        msgs.unshift(title);
+        return msgs.map(msg => "| " + msg).join("\n")
     }
 
-    export function error(...msgs: string[]) {
-        throw new Error(
-            view("@Artic / Error", ...msgs)
+
+    /**
+     * ### @Artic / Utils / Exceptions / Error
+     * 
+     * Quick method for dealing with Node callback
+     * error without having to test it in every use
+     * case to get coverage.
+     * @param err 
+     */
+    export function error(err: Error) {
+        if(err) { throw err }
+    }
+
+    /**
+     * ### @Artic / Utils / Exceptions / ArticError
+     * 
+     * The factory for creating an Artic specific
+     * error. It utilizes the template above for 
+     * developer readability.
+     * @param msgs 
+     */
+    export function articError(...msgs) {
+        error(
+            new Error(
+                view("@Artic / Error", ...msgs)
+            )
         )
     }
 
-    export function warning(...msgs: string[]) {
+    /**
+     * ### @Artic / Utils / Exceptions / ArticWarning
+     * 
+     * The factory for creating an Artic specific
+     * warning. It utilizes the template above for 
+     * developer readability.
+     * @param msgs 
+     */
+    export function articWarning(...msgs: string[]) {
         console.warn(
             view("@Artic / Warning", ...msgs)
         );

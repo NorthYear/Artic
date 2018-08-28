3f81f3ab487cf48e68c2dfd2da55bbe4bf069f76
Josh Miller <wickedhillsmedia@gmail.com>
2018-08-28 18:52:25 -0400

    update the database event store for managing event state in db

--- src/database.event.store.ts
+++ src/database.event.store.ts
@@ -1,9 +1,32 @@
 import { EventEmitter } from "events";
 
+/**
+ * ### @Artic / Database / DatabaseEventStore
+ * 
+ * Keeps up with event emitters for a particular 
+ * namespace in a database instance.
+ */
 export class DatabaseEventStore {
+    /**
+     * ### @Artic / Database / DatabaseEventStore / Events
+     * 
+     * Database wide events
+     */
     public events: EventEmitter = new EventEmitter();
+
+    /**
+     * ### @Artic / Database / DatabaseEventStore / Emitter
+     * 
+     * Individual namespace emitters for a database instance
+     */
     private emitters: { [key: string]: EventEmitter } = {}
 
+    /**
+     * ### @Artic / Database / DatabaseEventStore / For
+     * 
+     * Retrieve EventEmitter for an individual namespace
+     * @param namespace 
+     */
     public for(namespace: string) {
         if(this.emitters[namespace] === undefined) {
             this.emitters[namespace] = new EventEmitter();

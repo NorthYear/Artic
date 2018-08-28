1ecac71f07a6e2a0eeea1428912916658aedeeeb
Josh Miller <wickedhillsmedia@gmail.com>
2018-08-28 19:02:07 -0400

    Defined the database specific options interface

---
+++ src/interfaces/database.options.interface.ts
@@ -0,0 +1,9 @@
+import { AdapterInterface } from "./adapter.interface";
+
+export interface DatabaseOptionsInterface {
+    adapter?: AdapterInterface;
+    encryptionKey?: string;
+    context?: { [key: string]: new () => any };
+    hashNamespace?: boolean;
+    hashKeys?: boolean
+}

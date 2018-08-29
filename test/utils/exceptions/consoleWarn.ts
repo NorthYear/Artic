8bb674acb2094e42de6e3c9541217059e8014ac4
Josh Miller <wickedhillsmedia@gmail.com>
2018-08-29 16:24:35 -0400

    fixed exception test to not show warnings

--- test/utils/exceptions/consoleWarn.ts
+++ test/utils/exceptions/consoleWarn.ts
@@ -1,6 +1,8 @@
 import { Exceptions } from "../../../src/utils/exceptions";
-export function consoleWarn() {
-    let warn = jest.spyOn(console, "warn").mockImplementation();
+export function consoleWarn(done) {
+    let warn = jest.spyOn(console, "warn").mockImplementationOnce(() => {
+        done();
+    })
     Exceptions.articWarning(`Whoa!! You can't do that.`, `There is extra information you need to know about`);
     expect(warn).toBeCalled();
 }

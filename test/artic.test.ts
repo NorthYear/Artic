c1b3c7656af1f8a34de0bb42f914af5be6d412df
Josh Miller <wickedhillsmedia@gmail.com>
2018-08-29 16:25:25 -0400

    added validations to the core test suite

--- test/artic.test.ts
+++ test/artic.test.ts
@@ -4,10 +4,12 @@ import { utilsIsTesting } from "./utils/Is/testing";
 import { utilsExceptionsTesting } from "./utils/exceptions/testing";
 import { utilsCryptoboxTesting } from "./utils/cryptobox/testing";
 import { utilsSerializerTesting } from "./utils/serializer/testing";
+import { utilsValidationsTesting } from "./utils/validations/testing";
 
 describe("Utils", () => {
     describe("Is", utilsIsTesting)
     describe("Exceptions", utilsExceptionsTesting);
     describe("Cryptobox", utilsCryptoboxTesting);
     describe("Serializer", utilsSerializerTesting);
+    describe("Validations", utilsValidationsTesting);
 })

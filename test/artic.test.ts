5e159f9580db9f07fc127c0bdc18d370a0f637e4
Josh Miller <wickedhillsmedia@gmail.com>
2018-08-29 09:58:01 -0400

    added cryptobox and serialization testing to main suite

--- test/artic.test.ts
+++ test/artic.test.ts
@@ -1,8 +1,13 @@
 import "jest";
+require("dotenv").config();
 import { utilsIsTesting } from "./utils/Is/testing";
 import { utilsExceptionsTesting } from "./utils/exceptions/testing";
+import { utilsCryptoboxTesting } from "./utils/cryptobox/testing";
+import { utilsSerializerTesting } from "./utils/serializer/testing";
 
 describe("Utils", () => {
     describe("Is", utilsIsTesting)
     describe("Exceptions", utilsExceptionsTesting);
+    describe("Cryptobox", utilsCryptoboxTesting);
+    describe("Serializer", utilsSerializerTesting);
 })

5d60b03f09026652a00dbf1789da6d757bbf6775
Josh Miller <wickedhillsmedia@gmail.com>
2018-08-29 16:24:05 -0400

    fixed serialization test to not show warnings

--- test/utils/serializer/noConstructorFound.ts
+++ test/utils/serializer/noConstructorFound.ts
@@ -1,10 +1,14 @@
 import { Serializer } from "../../../src/utils/serializer";
 
-export function noConstructorFound() {
+export function noConstructorFound(done) {
+    let warn = jest.spyOn(console, "warn").mockImplementationOnce(() => {
+        done();
+    })
     expect(() => {
         var serializer = new Serializer({
             Broken: this
         }, "testing");
+        expect(warn).toBeCalled();
         serializer.parse('{"#artic-instance-type":"Broken"}');
     }).toThrow();
 }

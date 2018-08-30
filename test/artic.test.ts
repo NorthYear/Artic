5d6923f3b717fdedccbe15584d6e75625283a0e2
Josh Miller <wickedhillsmedia@gmail.com>
2018-08-30 01:22:16 -0400

    added parallel database instance to the core test suite

--- test/artic.test.ts
+++ test/artic.test.ts
@@ -6,6 +6,8 @@ import { utilsCryptoboxTesting } from "./utils/cryptobox/testing";
 import { utilsSerializerTesting } from "./utils/serializer/testing";
 import { utilsValidationsTesting } from "./utils/validations/testing";
 import { utilsBranderTesting } from "./utils/brander/testing";
+import { databaseTesting } from "./database/testing";
+import { databaseParallelInstanceTesting } from "./database.parallel.instance/testing";
 
 describe("Utils", () => {
     describe("Is", utilsIsTesting)
@@ -14,4 +16,9 @@ describe("Utils", () => {
     describe("Serializer", utilsSerializerTesting);
     describe("Brander", utilsBranderTesting);
     describe("Validations", utilsValidationsTesting);
+})
+
+describe("Database", () => {
+    describe("Database", databaseTesting);
+    describe("Database Parallel Instance", databaseParallelInstanceTesting);
 })

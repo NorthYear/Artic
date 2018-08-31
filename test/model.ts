8591a01554760dc72c2370a29c9aac6982b34aac
Josh Miller <wickedhillsmedia@gmail.com>
2018-08-30 22:31:27 -0400

    configed the model for testing

--- test/model.ts
+++ test/model.ts
@@ -1,5 +1,5 @@
-require('dotenv').config()
 import { Database, Entity } from "../src/index";
+import { DevAdapter } from "../src/adapters/dev.adapter";
 
 export class Article extends Entity {
     public title: string;
@@ -10,12 +10,22 @@ const context = {
     Article
 }
 
-export const mainDB = Database.oMake("main", {
+let mainAdapter = new DevAdapter();
+mainAdapter.quiet = true;
+
+let backupAdapter = new DevAdapter();
+backupAdapter.quiet = true;
+
+export const mainDB = Database.vMake("main", {
+    adapter: mainAdapter,
+    encryptionKey: process.env.ENCRYPTION_KEY,
     context
 })
 
-export const backupDB = Database.oMake("backup", {
+export const backupDB = Database.vMake("backup", {
+    adapter: backupAdapter,
+    encryptionKey: null,
     context
 })
 
-export const allDB = Database.oParallel(mainDB, backupDB);
+export const allDB = Database.vParallel(mainDB, backupDB);
